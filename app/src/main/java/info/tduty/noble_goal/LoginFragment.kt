b33fb2d388740be7599febecf3f4212c4a654f11
Evgeny Mezentsev <eimezentsev@yandex.ru>
2020-05-13 23:48:47 +0300

    Added base architecture

---
+++ app/src/main/java/info/tduty/noble_goal/LoginFragment.kt
@@ -0,0 +1,7 @@
+package info.tduty.noble_goal
+
+/**
+ * Created by Evgeniy Mezentsev on 13.05.2020.
+ */
+class LoginFragment {
+}
---
+++ app/src/main/java/info/tduty/noble_goal/MainActivity.kt
@@ -0,0 +1,8 @@
+package info.tduty.noble_goal
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Evgeniy Mezentsev on 12.05.2020.
+ */
+class MainActivity : AppCompatActivity(R.layout.activity_main)
